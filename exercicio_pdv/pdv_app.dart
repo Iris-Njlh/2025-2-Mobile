87bd03d90fee49a133e67484f5a833b47cefac8f
Iris-Njlh <iris.lima@uni9.edu.br>
2025-09-24 23:26:39 +0000

    Exercício 1: Sistema de Ponto de Venda (PDV)

---
+++ exercicio_pdv/pdv_app.dart
@@ -0,0 +1,19 @@
+import 'dart.io';
+import 'poduto.dart';
+import 'item_compra.dart';
+import 'carrinho_compra.dart';
+
+class PDVapp {
+    CarrinhoCompra carrinho = CarrinhoCompra ();
+
+    void iniciar () {
+        print ('\n== SISTEMA DE PDV\n');
+        print ('Bem-vindo ao sistema!');
+
+        bool continuar = true;
+
+        while (continuar) {
+            
+        }
+    }
+}
